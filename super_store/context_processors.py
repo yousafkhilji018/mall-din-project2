848e6e2b9fd26aa0e895ea667df51f307ec571bf
yousafkhilji018 <yousafkhilji018@gmail.com>
2026-09-13 13:01:08 +0500

    Fix Vercel 500: safe DB path, safe cart context, safe views, home template, accounts forms

--- super_store/context_processors.py
+++ super_store/context_processors.py
@@ -3,18 +3,21 @@ from store.models import Cart
 
 def cart_context(request):
     cart_item_count = 0
-    if request.user.is_authenticated:
-        try:
-            cart = Cart.objects.get(user=request.user)
-            cart_item_count = cart.get_item_count()
-        except Cart.DoesNotExist:
-            pass
-    else:
-        session_key = request.session.session_key
-        if session_key:
+    try:
+        if request.user.is_authenticated:
             try:
-                cart = Cart.objects.get(session_key=session_key)
+                cart = Cart.objects.get(user=request.user)
                 cart_item_count = cart.get_item_count()
             except Cart.DoesNotExist:
                 pass
+        else:
+            session_key = request.session.session_key
+            if session_key:
+                try:
+                    cart = Cart.objects.get(session_key=session_key)
+                    cart_item_count = cart.get_item_count()
+                except Cart.DoesNotExist:
+                    pass
+    except Exception:
+        cart_item_count = 0
     return {'cart_item_count': cart_item_count}
